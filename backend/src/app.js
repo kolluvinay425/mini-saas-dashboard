8eef62c0ee597d467efce061cc1adc1028bc8cfc
Vinay <kolluvinay425@gmail.com>
2026-07-16 03:02:50 +0200

    Add project API routes and register project router

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
+import projectRoutes from "./routes/project.route.js";
 
 const app = express();
 
@@ -9,6 +10,8 @@ app.use(cors());
 app.use(helmet());
 app.use(morgan("dev"));
 app.use(express.json());
+
+app.use("/api/projects", projectRoutes);
 
 app.get("/api/health", (req, res) => {
   res.json({
